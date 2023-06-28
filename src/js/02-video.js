20a043406feff430afe1577c03f4eaa97a879378
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-06-28 14:39:59 +0300

    add 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+const fixedTime = localStorage.getItem('videoplayer-current-time');
+
+player.on('timeupdate', throttle(timeUpdateHandler, 1000));
+
+function timeUpdateHandler(e){
+	const currentTime = e.seconds;
+	localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+if (fixedTime) {
+	player.setCurrentTime(fixedTime);
+}
+// console.log(fixedTime);
